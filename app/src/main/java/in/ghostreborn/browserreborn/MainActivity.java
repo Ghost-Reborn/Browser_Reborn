b48f9857807273860f8df917afbe0589b943fff8
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2021-12-18 20:26:20 +0530

    Removed Layout XML layout is removed, the layout is generated programmatically

--- app/src/main/java/in/ghostreborn/browserreborn/MainActivity.java
+++ app/src/main/java/in/ghostreborn/browserreborn/MainActivity.java
@@ -3,12 +3,14 @@ package in.ghostreborn.browserreborn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        WebView rebornWebView = new WebView(this);
+        setContentView(rebornWebView);
     }
 }
