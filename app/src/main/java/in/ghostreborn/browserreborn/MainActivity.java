7dc74cdcddb768197d6aee8e00eb23013753a035
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2021-12-18 20:32:59 +0530

    Browser Now this app is a browser officially

--- app/src/main/java/in/ghostreborn/browserreborn/MainActivity.java
+++ app/src/main/java/in/ghostreborn/browserreborn/MainActivity.java
@@ -3,7 +3,9 @@ package in.ghostreborn.browserreborn;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         WebView rebornWebView = new WebView(this);
         setContentView(rebornWebView);
+
+        rebornWebView.loadUrl("https://www.google.com");
+        rebornWebView.getSettings().setJavaScriptEnabled(true);
+        rebornWebView.canGoBack();
+        rebornWebView.setWebViewClient(new RebornWebViewClient());
+
+    }
+}
+
+class RebornWebViewClient extends WebViewClient{
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
+        return true;
     }
 }
